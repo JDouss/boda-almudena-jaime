55e8a182b927bb8c2a3ef73d04a6c6a1238e86b1
JDouss <47002127+JDouss@users.noreply.github.com>
2025-06-16 22:59:34 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/https://github.com/JDouss/boda-almudena-jaime/", // <-- REEMPLAZA ESTO
+  base: "/boda-almudena-jaime/", // <-- REEMPLAZA ESTO
   server: {
     host: "::",
     port: 8080,
